6b499f21d6b49b7836b9a519686c21071cd98812
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-05 16:03:36 -0300

    Update LinPonRetangulos.c

--- LinPonRetangulos.c
+++ LinPonRetangulos.c
@@ -56,9 +56,9 @@ SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
 SDL_RenderDrawLine (ren,200,100, 150,150);
 /*ponto*/
 SDL_SetRenderDrawColor(ren, 0xFF,0x00,0x00,0x00); 
-SDL_RenderDrawPoint(ren*ren, 100,50);
+SDL_RenderDrawPoint(ren, 100,50);
 SDL_SetRenderDrawColor(ren, 0xFF,0x00,0xFF,0x00); 
-SDL_RenderDrawPoint(ren*ren,300,150);
+SDL_RenderDrawPoint(ren, 300,150);
 SDL_SetRenderDrawColor(ren, 0x00,0xFF,0x00,0x00); 
 SDL_RenderDrawPoint(ren, 100,250);
 SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00); 
