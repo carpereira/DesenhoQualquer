ee1af80c9a6b5f4362133eb79d890eeae4851928
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-05 15:36:01 -0300

    Update Pontos.c

--- Pontos.c
+++ Pontos.c
@@ -15,6 +15,22 @@ SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
 SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
 SDL_RenderClear(ren);
 
+for (i=0; i<400;i++){
+  for (j=0; j<400; j++)
+  {
+    if (map.at(i).at(j) == 1)
+    {
+      SDL_SetRenderDrawColor(renderer, 50,255,90,255);
+      SDL_RenderDrawPoint(renderer,j,i);
+    }
+    else
+    {
+      SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+      SDL_RenderDrawPoint(renderer,j,i);
+    }
+  }
+}
+     
 /*ponto*/
 SDL_SetRenderDrawColor(ren, 0xFF,0x00,0x00,0x00); 
 SDL_RenderDrawPoint(ren, 100,50);
